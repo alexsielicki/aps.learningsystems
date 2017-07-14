0e0a32276505a7a1e9089fe5894913475bb026b4
Alex M. Sielicki <alex@techmilo.com>
2017-07-14 22:20:01 +0900

    Removing tasks

--- src/aps/learningsystems/interfaces.py
+++ src/aps/learningsystems/interfaces.py
@@ -11,14 +11,3 @@ class IApsLearningsystemsLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
 
 
-class ITask(Interface):
-
-    title = schema.TextLine(
-        title=_(u"Title"),
-        required=True,
-    )
-
-    description = schema.Text(
-        title=_(u"Description"),
-        required=False,
-    )
